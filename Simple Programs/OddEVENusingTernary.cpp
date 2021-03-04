76f788c1835f59beb59698d0d20714ecdb57d7a0
Aniket Marwade <65933448+aniketmarwade@users.noreply.github.com>
2021-03-04 17:08:31 +0530

    one

---
+++ Simple Programs/OddEVENusingTernary.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"Enter any no";
+	cin>>n;
+	
+//	(n%2==0) ? cout<<"Even"; : cout<<"Odd";
+	(n%2==0) ? cout<<"Even"; : cout<<"Odd";
+	return 0;
+	
+}
